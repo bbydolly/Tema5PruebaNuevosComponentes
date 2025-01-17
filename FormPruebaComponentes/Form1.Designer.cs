cee7119714af9458170260b81b4c30ec44e61fef
bbydolly <cristinalopezdelcastilloinf1@gmail.com>
2025-01-17 13:01:34 +0100

    Agregar archivos de proyecto.

---
+++ FormPruebaComponentes/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace FormPruebaComponentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelTextBox1 = new Tema5PruebaNuevosComponentes.LabelTextBox();
+            this.etiquetaAviso1 = new Tema5PruebaNuevosComponentes.EtiquetaAviso();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Change position";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Incrementar separación";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelTextBox1
+            // 
+            this.labelTextBox1.Location = new System.Drawing.Point(28, 41);
+            this.labelTextBox1.Name = "labelTextBox1";
+            this.labelTextBox1.Posicion = Tema5PruebaNuevosComponentes.LabelTextBox.EPosicion.IZQUIERDA;
+            this.labelTextBox1.PswChr = 'u';
+            this.labelTextBox1.Separacion = 0;
+            this.labelTextBox1.Size = new System.Drawing.Size(135, 20);
+            this.labelTextBox1.TabIndex = 0;
+            this.labelTextBox1.TextLbl = "label1";
+            this.labelTextBox1.TextTxt = "";
+            this.labelTextBox1.PosicionChanged += new System.EventHandler(this.labelTextBox1_PosicionChanged);
+            this.labelTextBox1.TxtChange += new System.EventHandler(this.labelTextBox1_TxtChange_1);
+            this.labelTextBox1.SeparacionChanged += new System.EventHandler(this.labelTextBox1_SeparacionChanged_1);
+            this.labelTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.labelTextBox1_KeyPress);
+            this.labelTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.labelTextBox1_KeyUp);
+            // 
+            // etiquetaAviso1
+            // 
+            this.etiquetaAviso1.Location = new System.Drawing.Point(43, 135);
+            this.etiquetaAviso1.Marca = Tema5PruebaNuevosComponentes.EMarca.Cruz;
+            this.etiquetaAviso1.Name = "etiquetaAviso1";
+            this.etiquetaAviso1.Size = new System.Drawing.Size(97, 15);
+            this.etiquetaAviso1.TabIndex = 3;
+            this.etiquetaAviso1.Text = "etiquetaAviso1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.etiquetaAviso1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tema5PruebaNuevosComponentes.LabelTextBox labelTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Tema5PruebaNuevosComponentes.EtiquetaAviso etiquetaAviso1;
+    }
+}
+
---
+++ FormPruebaComponentes/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tema5PruebaNuevosComponentes;
+
+namespace FormPruebaComponentes
+{
+
+    //Si el set está mal puede saltar stackOverFlow (Llamada recursiva)
+    //, editar la clase con 
+    //Para pruebas 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /* Prueba para ver que se ha enlazado en componente txt al LabelTextBox
+             */
+            Debug.WriteLine("Key pressed");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (labelTextBox1.Posicion == LabelTextBox.EPosicion.IZQUIERDA)
+            {//Izquierda
+                labelTextBox1.Posicion = LabelTextBox.EPosicion.DERECHA;
+
+            }
+            else
+            {
+                labelTextBox1.Posicion = LabelTextBox.EPosicion.IZQUIERDA;
+
+            }
+
+        }
+
+        private void labelTextBox1_PosicionChanged(object sender, EventArgs e)
+        {
+            this.Text = labelTextBox1.Posicion.ToString();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelTextBox1.Separacion += 5;
+
+        }
+
+        //Hay que enlazarlo con el evento desde la definicion de LabelTextBox en el momento en el que se modifica la separacion
+        //en el set y se recolocan los elementos lanzo el evento
+        private void labelTextBox1_SeparacionChanged_1(object sender, EventArgs e)
+        {
+            Debug.Write(labelTextBox1.Separacion);
+
+        }
+
+        private void labelTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine("Levanto la tecla");
+        }
+
+
+        //En UserControl he tenido que invocar el evento, hacer la llamada
+        private void labelTextBox1_TxtChange_1(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Textchange");
+        }
+
+        //Método que dibuja en el canvas, región del cliente
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //Debug.Write("pinto");
+            ////Lienzo donde puedo empezar a pintar
+            ////La representación gráfica de un string, No es un objeto 
+            //e.Graphics.DrawString("Prueba de escritura de texto",
+            //this.Font, Brushes.BlueViolet, 10, 10); //la opacidad?
+            //Mirar brocha 
+            //Paint está en control
+            //No se recomìla
+
+        }
+    }
+}
