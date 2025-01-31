0966a27dce69be9e6b866cd51ec26a7455d6e176
bbydolly <cristinalopezdelcastilloinf1@gmail.com>
2025-01-31 11:18:47 +0100

    Todo acabar 2 bitmap , validar 1

--- FormPruebaComponentes/Form1.Designer.cs
+++ FormPruebaComponentes/Form1.Designer.cs
@@ -30,13 +30,19 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.etiquetaAviso4 = new Tema5PruebaNuevosComponentes.EtiquetaAviso();
+            this.etiquetaAviso3 = new Tema5PruebaNuevosComponentes.EtiquetaAviso();
+            this.etiquetaAviso2 = new Tema5PruebaNuevosComponentes.EtiquetaAviso();
             this.etiquetaAviso1 = new Tema5PruebaNuevosComponentes.EtiquetaAviso();
             this.labelTextBox1 = new Tema5PruebaNuevosComponentes.LabelTextBox();
+            this.etiquetaAviso5 = new Tema5PruebaNuevosComponentes.EtiquetaAviso();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 87);
+            this.button1.Location = new System.Drawing.Point(16, 87);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 23);
             this.button1.TabIndex = 1;
@@ -46,7 +52,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(179, 87);
+            this.button2.Location = new System.Drawing.Point(136, 87);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(142, 23);
             this.button2.TabIndex = 2;
@@ -54,9 +60,56 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Práctica guiada:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ejercicio 2:";
+            // 
+            // etiquetaAviso4
+            // 
+            this.etiquetaAviso4.Gradiente = false;
+            this.etiquetaAviso4.Location = new System.Drawing.Point(69, 304);
+            this.etiquetaAviso4.Marca = Tema5PruebaNuevosComponentes.EMarca.Imagen;
+            this.etiquetaAviso4.Name = "etiquetaAviso4";
+            this.etiquetaAviso4.Size = new System.Drawing.Size(0, 0);
+            this.etiquetaAviso4.TabIndex = 8;
+            // 
+            // etiquetaAviso3
+            // 
+            this.etiquetaAviso3.Gradiente = false;
+            this.etiquetaAviso3.Location = new System.Drawing.Point(69, 317);
+            this.etiquetaAviso3.Marca = Tema5PruebaNuevosComponentes.EMarca.Imagen;
+            this.etiquetaAviso3.Name = "etiquetaAviso3";
+            this.etiquetaAviso3.Size = new System.Drawing.Size(0, 0);
+            this.etiquetaAviso3.TabIndex = 7;
+            // 
+            // etiquetaAviso2
+            // 
+            this.etiquetaAviso2.Gradiente = true;
+            this.etiquetaAviso2.Location = new System.Drawing.Point(45, 260);
+            this.etiquetaAviso2.Marca = Tema5PruebaNuevosComponentes.EMarca.Cruz;
+            this.etiquetaAviso2.Name = "etiquetaAviso2";
+            this.etiquetaAviso2.Size = new System.Drawing.Size(97, 15);
+            this.etiquetaAviso2.TabIndex = 4;
+            this.etiquetaAviso2.Text = "etiquetaAviso2";
+            // 
             // etiquetaAviso1
             // 
-            this.etiquetaAviso1.Location = new System.Drawing.Point(43, 135);
+            this.etiquetaAviso1.Gradiente = false;
+            this.etiquetaAviso1.Location = new System.Drawing.Point(28, 135);
             this.etiquetaAviso1.Marca = Tema5PruebaNuevosComponentes.EMarca.Cruz;
             this.etiquetaAviso1.Name = "etiquetaAviso1";
             this.etiquetaAviso1.Size = new System.Drawing.Size(97, 15);
@@ -81,11 +134,27 @@
             this.labelTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.labelTextBox1_KeyPress);
             this.labelTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.labelTextBox1_KeyUp);
             // 
+            // etiquetaAviso5
+            // 
+            this.etiquetaAviso5.Gradiente = false;
+            this.etiquetaAviso5.Location = new System.Drawing.Point(136, 331);
+            this.etiquetaAviso5.Marca = Tema5PruebaNuevosComponentes.EMarca.Imagen;
+            this.etiquetaAviso5.Name = "etiquetaAviso5";
+            this.etiquetaAviso5.Size = new System.Drawing.Size(78, 13);
+            this.etiquetaAviso5.TabIndex = 9;
+            this.etiquetaAviso5.Text = "etiquetaAviso5";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.etiquetaAviso5);
+            this.Controls.Add(this.etiquetaAviso4);
+            this.Controls.Add(this.etiquetaAviso3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.etiquetaAviso2);
             this.Controls.Add(this.etiquetaAviso1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -94,6 +163,7 @@
             this.Text = "Practica guiada Dibujo GDI y clase Control";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,6 +173,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private Tema5PruebaNuevosComponentes.EtiquetaAviso etiquetaAviso1;
+        private Tema5PruebaNuevosComponentes.EtiquetaAviso etiquetaAviso2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Tema5PruebaNuevosComponentes.EtiquetaAviso etiquetaAviso3;
+        private Tema5PruebaNuevosComponentes.EtiquetaAviso etiquetaAviso4;
+        private Tema5PruebaNuevosComponentes.EtiquetaAviso etiquetaAviso5;
     }
 }
 
--- Tema5PruebaNuevosComponentes/EtiquetaAviso.cs
+++ Tema5PruebaNuevosComponentes/EtiquetaAviso.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,15 @@ using System.Windows.Forms;
 
 namespace Tema5PruebaNuevosComponentes
 {
+    //Etiqueta Aviso
 
     //Define que vamos a dibujar (fuera de la clase)
     public enum EMarca
     {
         Nada,
         Cruz,
-        Circulo
+        Circulo,
+        Imagen //ej2
     }
 
     public partial class EtiquetaAviso : Control
@@ -36,6 +39,7 @@ namespace Tema5PruebaNuevosComponentes
         //Defino una nueva propiedad para el componente CustomControl
 
         private EMarca marca = EMarca.Nada;
+        //Va encima de la propiedad pública, que es la que se puede ver en propiedades
         [Category("Appearance")]
         [Description("Indica el tipo de marca que aparece junto al texto")]
         public EMarca Marca
@@ -54,6 +58,46 @@ namespace Tema5PruebaNuevosComponentes
         }
 
 
+
+        //todo no puedo hacer el set 
+        private Bitmap imagenMarca;
+
+        [Category("Appearence")]
+        [Description("Determina la imagen que se va a cargar")]
+        public Bitmap ImagenMarca
+        {
+            set
+            {
+                imagenMarca = value;
+            }
+            get
+            {
+                return imagenMarca;
+            }
+        }
+
+
+
+        //Ej 2 fondo opcional gradiente entre dos colores
+        //Defino la propiedad 
+        private bool gradiente;
+        [Category("Appearence")]
+        [Description("Establece un fondo de gradient entre dos colores")]
+        public bool Gradiente
+        {
+            set
+            {
+                gradiente = value;
+            }
+            get
+            {
+                return gradiente;
+            }
+        }
+
+
+
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
@@ -67,6 +111,32 @@ namespace Tema5PruebaNuevosComponentes
             //Esta propiedad provoca mejoras en la apariencia o en la eficiencia
             // a la hora de dibujar
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+
+
+            //El orden de pintado correcto es:
+            //1.Fondo
+            //2.Figura
+            //3.Texto
+
+            //Por ello compruebo primero el gradiente
+            if (Gradiente)
+            {   //PASOS PARA DIBUJAR
+                //1.Primero pintar la forma (en este caso un rectángulo)
+                //Coge todo el componente desde 0,0
+
+                //Definición de la figura a dibujar, y desde donde empieza, coordenadas de origen 0,0
+                //ancho y alto de la propia etiqueta
+                Rectangle r = new Rectangle(0, 0, this.Width, this.Height);
+                //2. Usar LinearGradientBrush para pintar el gradiente
+                Brush brush = new LinearGradientBrush(r, Color.AliceBlue, Color.Red, 1);
+                g.FillRectangle(brush, r);
+            }
+
+
+
+
+            //2.Pinto figura
             //Dependiendo del valor de la propiedad marca dibujamos una
             //Cruz o un Círculo
             switch (Marca)
@@ -89,9 +159,25 @@ namespace Tema5PruebaNuevosComponentes
                     //pueden realizar muchos y cogen memoria
                     lapiz.Dispose();
                     break;
+                case EMarca.Imagen:
+
+                    ImagenMarca(new Bitmap("C:\\Users\\Cris\\Desktop\\MuchasFotos\\f1.jpg"));
+                    g.DrawImage(new Bitmap("C:\\Users\\Cris\\Desktop\\MuchasFotos\\f1.jpg"), 0, 0, h, h);
+                    offsetX = h; //Cuanto se desplazan las letras en el eje x
+                    break;
+
             }
+
+
+
+
+
+
+
+            //3.Pinto texto
             //Finalmente pintamos el Texto; desplazado si fuera necesario
             SolidBrush b = new SolidBrush(this.ForeColor);
+
             g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
             Size tam = g.MeasureString(this.Text, this.Font).ToSize();
             this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY * 2);
